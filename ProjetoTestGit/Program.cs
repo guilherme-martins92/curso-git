eb3c01122a8848995bc30c29b63eafe72c355653
Guilherme Martins <guilherme.martins92@outlook.com>
2021-04-12 15:51:10 -0300

    Acrescentado a frase desejando um bom dia

--- ProjetoTestGit/Program.cs
+++ ProjetoTestGit/Program.cs
@@ -7,8 +7,8 @@ namespace ProjetoTestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia! ");
             Console.WriteLine("Como voce esta? hoje");
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
