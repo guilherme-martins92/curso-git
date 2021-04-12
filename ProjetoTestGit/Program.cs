41e5043af9d950be5d0b9ce7ae34b91fabbacc5c
Guilherme Martins <martins_gui92@hotmail.com>
2021-04-12 14:40:02 -0300

    Projeto Iniciado monstrado a msg ola mundo

---
+++ ProjetoTestGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoTestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
