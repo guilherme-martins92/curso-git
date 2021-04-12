fde350db1d92b1b5886a6c729c507e4d8b72847d
Guilherme Martins <martins_gui92@hotmail.com>
2021-04-12 15:03:21 -0300

    pergunta como vc esta

--- ProjetoTestGit/Program.cs
+++ ProjetoTestGit/Program.cs
@@ -8,6 +8,7 @@ namespace ProjetoTestGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia! ");
+            Console.WriteLine("Como voce esta? hoje");
         }
     }
 }
