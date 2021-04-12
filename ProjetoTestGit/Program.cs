0fbd072e0ccb03b77d4efa3ae63b16c9dc1679b7
Guilherme Felipe Martins <guilherme.martins92@outlook.com>
2021-04-12 15:55:20 -0300

    Frase de teste acrescentada

--- ProjetoTestGit/Program.cs
+++ ProjetoTestGit/Program.cs
@@ -9,6 +9,7 @@ namespace ProjetoTestGit
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como voce esta? hoje");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Fazendo mais um teste do github");
         }
     }
 }
